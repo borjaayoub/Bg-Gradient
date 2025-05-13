2cc6e2bbf3a52c6f8457eb35b1b67dbf0bf709b7
ayoub <ayoubborja@gmail.com>
2025-05-13 15:34:30 +0100

    id name change

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 const color1 = document.querySelector('#color1')
 const color2 = document.querySelector('#color2')
 const body = document.querySelector('body');
-const css = document.querySelector('#css')
+const gradientText = document.querySelector('#gradientText')
 
 body.addEventListener('input', function(){
-    console.log(body.style.backgroundImage);
-
     body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
-    css.textContent = body.style.background;
+    gradientText.textContent = body.style.background;    
 })
-
-
